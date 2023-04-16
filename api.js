a995576abd9f9b0429787923bf7b5bd03b931ac1
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-16 14:42:04 +0200

    declare global variables currenciesURL & countriesURL

--- api.js
+++ api.js
@@ -4,16 +4,12 @@ var countriesURL =
   "https://gist.githubusercontent.com/tiagodealmeida/0b97ccf117252d742dddf098bc6cc58a/raw/f621703926fc13be4f618fb4a058d0454177cceb/countries.json";
 
 async function getCurrenciesJSON() {
-  let response = await fetch(
-    "https://gist.githubusercontent.com/ksafranski/2973986/raw/5fda5e87189b066e11c1bf80bbfbecb556cf2cc1/Common-Currency.json"
-  );
+  let response = await fetch(currenciesURL);
   return await response.json();
 }
 
 async function getCountriesJSON() {
-  let response = await fetch(
-    "https://gist.githubusercontent.com/tiagodealmeida/0b97ccf117252d742dddf098bc6cc58a/raw/f621703926fc13be4f618fb4a058d0454177cceb/countries.json"
-  );
+  let response = await fetch(countriesURL);
   return await response.json();
 }
 
