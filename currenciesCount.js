fae4cc6b9f93bd4c7d2d3d62434284319a7f4939
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 12:15:31 +0200

    made the functions one line

--- currenciesCount.js
+++ currenciesCount.js
@@ -5,28 +5,22 @@ let countries = countriesJson.countries.country;
 
 // function to get all currencies from countries.json without duplicates (returns array) [1]
 
-let getCurrenciesSet = (countries) => {
-  return [...new Set(countries.map((countries) => countries.currencyCode))];
-};
+let getCurrenciesSet = (countries) => [
+  ...new Set(countries.map((countries) => countries.currencyCode)),
+];
 
 // function to get all currencies from countries.json without duplicates (returns object) [2]
 
-let getCurrenciesMap = (countries) => {
-  //filter out the duplicates and return map of currencies
-  return countries
+let getCurrenciesMap = (countries) =>
+  countries
     .filter(
       (value, index, self) =>
         self.findIndex((t) => t.currencyCode === value.currencyCode) === index
     )
     .map((countries) => countries.currencyCode);
-};
 
-export let getCurrenciesWithoutDuplicatesUsingSet = () => {
-  let currencies = getCurrenciesSet(countries);
-  return currencies;
-};
+export let getCurrenciesWithoutDuplicatesUsingSet = () =>
+  getCurrenciesSet(countries);
 
-export let getCurrenciesWithoutDuplicateUsingMap = () => {
-  let currencies = getCurrenciesMap(countries);
-  return currencies;
-};
+export let getCurrenciesWithoutDuplicateUsingMap = () =>
+  getCurrenciesMap(countries);
