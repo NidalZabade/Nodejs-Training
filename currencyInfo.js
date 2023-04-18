a4fb4d97c537edbf7c1ecf14c18dd80074794e8e
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 12:16:25 +0200

    replaced filter with find

--- currencyInfo.js
+++ currencyInfo.js
@@ -6,15 +6,17 @@ let currencies = await getCurrenciesJSON();
 // Get the currency object by name or code
 let searchForCurrency = (currency) => {
   // The currency code is always in uppercase
-  var code = currency.toUpperCase();
+  let code = currency.toUpperCase();
   if (currencies[code]) {
     return currencies[code];
   }
-  currency = currency.toLowerCase();
-  let currencyName = Object.values(currencies);
-  return currencyName.filter(
-    (currencyName) => currencyName.name.toLowerCase() === currency
-  )[0];
+  let currencyName = currency.toLowerCase();
+  let currenciesName = Object.values(currencies);
+  return (
+    currenciesName.find(
+      (currency) => currency.name.toLowerCase() === currencyName
+    ) ?? CURRENCY_NOT_FOUND
+  );
 };
 
-export let getCurrency = (name) => (searchForCurrency(name) ?? CURRENCY_NOT_FOUND);
+export let getCurrency = (name) => searchForCurrency(name);
