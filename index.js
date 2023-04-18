9bbecb85fd2f43126df450ff6e3c09bfe490db62
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 12:16:52 +0200

    test new changes

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import { getCountry } from "./countryInfo.js";
+import {
+  getCountry,
+  searchForCountryUsingWhileLoop,
+  searchForCountryUsingForLoop,
+  searchForCountryUsingForOfLoop,
+  searchForCountryUsingForInLoop
+} from "./countryInfo.js";
 import { getCurrency } from "./currencyInfo.js";
-import { getCurrenciesWithoutDuplicateUsingMap,getCurrenciesWithoutDuplicatesUsingSet } from "./currenciesCount.js";
+import {
+  getCurrenciesWithoutDuplicateUsingMap,
+  getCurrenciesWithoutDuplicatesUsingSet,
+} from "./currenciesCount.js";
 import { LINE } from "./constants.js";
 
 console.log(LINE);
 console.log(getCountry("India"));
+console.log(LINE);
+console.log(searchForCountryUsingWhileLoop("India"));
+console.log(LINE);
+console.log(searchForCountryUsingForLoop("India"));
+console.log(LINE);
+console.log(searchForCountryUsingForOfLoop("India"));
+console.log(LINE);
+console.log(searchForCountryUsingForInLoop("India"));
 console.log(LINE);
 console.log(getCurrency("INR"));
 console.log(LINE);
 console.log(getCurrenciesWithoutDuplicateUsingMap());
 console.log(LINE);
 console.log(getCurrenciesWithoutDuplicatesUsingSet());
-
