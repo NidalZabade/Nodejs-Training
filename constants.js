61be08a8affc9588cfb2914285701e542da24f4a
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 10:39:27 +0200

    added constants file .e.g (CURRENCIES_URL,...)

---
+++ constants.js
@@ -0,0 +1,7 @@
+export let CURRENCIES_URL =
+  "https://gist.githubusercontent.com/ksafranski/2973986/raw/5fda5e87189b066e11c1bf80bbfbecb556cf2cc1/Common-Currency.json";
+export let COUNTRIES_URL =
+  "https://gist.githubusercontent.com/tiagodealmeida/0b97ccf117252d742dddf098bc6cc58a/raw/f621703926fc13be4f618fb4a058d0454177cceb/countries.json";
+export let COUNTRY_NOT_FOUND = "Country not found";
+export let CURRENCY_NOT_FOUND = "Currency not found";
+export let LINE= "----------------------------------------";
