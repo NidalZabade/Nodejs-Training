1b859dda74c949894db9f849dd8e684c9fe8a469
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 10:52:41 +0200

    used the new renamed functions

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { getCountryInfo } from "./countryInfo.js";
-import { getCurrencyInfo } from "./currencyInfo.js";
-import { getCurrenciesCountUsingSet,getCurrenciesCountUsingMap } from "./currenciesCount.js";
+import { getCountry } from "./countryInfo.js";
+import { getCurrency } from "./currencyInfo.js";
+import { getCurrenciesWithoutDuplicateUsingMap,getCurrenciesWithoutDuplicatesUsingSet } from "./currenciesCount.js";
+import { LINE } from "./constants.js";
 
-
-getCountryInfo("India");
-getCurrencyInfo("INR");
-getCurrenciesCountUsingSet();
-getCurrenciesCountUsingMap();
+console.log(LINE);
+console.log(getCountry("India"));
+console.log(LINE);
+console.log(getCurrency("INR"));
+console.log(LINE);
+console.log(getCurrenciesWithoutDuplicateUsingMap());
+console.log(LINE);
+console.log(getCurrenciesWithoutDuplicatesUsingSet());
 
