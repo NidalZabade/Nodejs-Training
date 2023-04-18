78271d3120656e15859b713fbca739d6d36969f6
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-18 12:14:36 +0200

    Replaced filter with find,made the functions one line and added 4 functions each used one type of loops to search for a country

--- countryInfo.js
+++ countryInfo.js
@@ -2,16 +2,71 @@ import { getCountriesJSON } from "./api.js";
 import { COUNTRY_NOT_FOUND } from "./constants.js";
 
 let countriesJson = await getCountriesJSON();
-
+let countries = countriesJson.countries.country;
 // Get the country object by name or capital
 let searchForCountry = (name) => {
   name = name.toLowerCase();
-  var countries = countriesJson.countries.country;
-  return countries.filter(
-    (countries) =>
-      countries.countryName.toLowerCase() === name ||
-      countries.capital.toLowerCase() === name
-  )[0];
+  return (
+    countries.find(
+      (country) =>
+        country.countryName.toLowerCase() === name ||
+        country.capital.toLowerCase() === name
+    ) ?? COUNTRY_NOT_FOUND
+  );
 };
 
-export let getCountry = (name) => searchForCountry(name) ?? COUNTRY_NOT_FOUND;
+export let searchForCountryUsingWhileLoop = (name) => {
+  name = name.toLowerCase();
+  let i = 0;
+  while (i < countries.length) {
+    if (
+      countries[i].countryName.toLowerCase() === name ||
+      countries[i].capital.toLowerCase() === name
+    ) {
+      return countries[i];
+    }
+    i++;
+  }
+  return COUNTRY_NOT_FOUND;
+};
+
+export let searchForCountryUsingForLoop = (name) => {
+  name = name.toLowerCase();
+  for (let i = 0; i < countries.length; i++) {
+    if (
+      countries[i].countryName.toLowerCase() === name ||
+      countries[i].capital.toLowerCase() === name
+    ) {
+      return countries[i];
+    }
+  }
+  return COUNTRY_NOT_FOUND;
+};
+
+export let searchForCountryUsingForOfLoop = (name) => {
+  name = name.toLowerCase();
+  for (let country of countries) {
+    if (
+      country.countryName.toLowerCase() === name ||
+      country.capital.toLowerCase() === name
+    ) {
+      return country;
+    }
+  }
+  return COUNTRY_NOT_FOUND;
+};
+
+export let searchForCountryUsingForInLoop = (name) => {
+  name = name.toLowerCase();
+  for (let i in countries) {
+    if (
+      countries[i].countryName.toLowerCase() === name ||
+      countries[i].capital.toLowerCase() === name
+    ) {
+      return countries[i];
+    }
+  }
+  return COUNTRY_NOT_FOUND;
+};
+
+export let getCountry = (name) => searchForCountry(name);
