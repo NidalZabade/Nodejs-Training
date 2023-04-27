da90c057430fd43e55610b7b09a58733a6406f36
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-27 10:11:26 +0200

    Test all functions

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ import {
   getCurrenciesWithoutDuplicatesUsingSet,
 } from "./currenciesCount.js";
 import { LINE } from "./constants.js";
+import { Stack } from "./stack.js";
 
+
+// test CurrencyInfo, CountryInfo and CurrenciesCount modules
 console.log(LINE);
 console.log(getCountry("India"));
 console.log(LINE);
@@ -28,3 +31,32 @@ console.log(LINE);
 console.log(getCurrenciesWithoutDuplicateUsingMap());
 console.log(LINE);
 console.log(getCurrenciesWithoutDuplicatesUsingSet());
+
+
+// test Stack class
+const stack = new Stack();
+console.log(stack.isEmpty());
+console.log(stack.displayStack(stack));
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+stack.push(6);
+console.log(stack.displayStack(stack));
+console.log(stack.pop());
+console.log(stack.trim());
+console.log(stack.isEmpty());
+console.log(stack.size());
+console.log(stack.peek());
+console.log(stack.displayStack(stack));
+console.log(stack.reverse_stack(stack));
+console.log(stack.max());
+console.log(stack.min());
+console.log(stack.find(3));
+console.log(stack.set());
+console.log(stack.top(2));
+console.log(stack.bottom(2));
+console.log(stack.remove(2));
+console.log(stack.items);
+console.log(stack.clear());
