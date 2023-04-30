3961c339d8c34042babe8a9cefefb8d814de108e
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-04-30 10:03:23 +0300

    fixed date functions

--- customDate.js
+++ customDate.js
@@ -1,4 +1,11 @@
-import { WEEK_DAYS } from "./constants.js";
+import {
+  WEEK_DAYS,
+  DAY_IN_MILLISECOND,
+  MINUTES_IN_ONE_HOUR,
+  MONTHS_IN_QUARTER,
+  INCREMENT_ONE_DAY,
+  DECREMENT_ONE_DAY,
+} from "./constants.js";
 export class CustomDate {
   constructor() {
     this.now = new Date();
@@ -9,25 +16,28 @@ export class CustomDate {
   updateDate(newDate) {
     this.date = new Date(newDate);
   }
+  getNewDate(daysDifference) {
+    return new Date(this.date).setDate(this.date.getDate() + daysDifference);
+  }
 
   // return yesterday's date
   yesterday() {
-    return new Date(new Date(this.date).setDate(this.date.getDate() - 1));
+    return new Date(this.getNewDate(DECREMENT_ONE_DAY));
   }
 
   // return yesterday's day
   yesterdayDay() {
-    return WEEK_DAYS[new Date(new Date(this.date).setDate(this.date.getDate() - 1)).getDay()];
+    return WEEK_DAYS[this.yesterday().getDay()];
   }
 
   // return tomorrow's date
   tomorrow() {
-    return new Date(new Date(this.date).setDate(this.date.getDate() + 1));
+    return new Date(this.getNewDate(INCREMENT_ONE_DAY));
   }
 
   // return tomorrow's day
   tomorrowDay() {
-    return WEEK_DAYS[new Date(new Date(this.date).setDate(this.date.getDate() + 1)).getDay()];
+    return WEEK_DAYS[this.tomorrow().getDay()];
   }
 
   // add minutes to this.date
@@ -50,7 +60,7 @@ export class CustomDate {
 
   // Logs the current date-time
   current() {
-    return this.now;
+    return this.date;
   }
 
   // return boolean
@@ -60,40 +70,43 @@ export class CustomDate {
 
   // Return (bigger, smaller or equal) between the given date and this.date
   compare(newDate) {
-    if (this.date > newDate) {
-      return "bigger";
-    } else if (this.date < newDate) {
-      return "smaller";
+    if (this.date === newDate) {
+      return "equal";
     }
-    return "equal";
+    return this.date > newDate ? "bigger" : "smaller";
   }
 
   // Return the difference between the given date and this.date in format (x Days, y hours, z minutes)
   differenceInDays(newDate) {
-    return `${Math.floor((new Date(newDate) - this.date) / (1000 * 60 * 60 * 24))} Days`;
+    return `${Math.floor(
+      (new Date(newDate) - this.date) / DAY_IN_MILLISECOND
+    )} Days`;
   }
 
   // Returns minutes in hours format, eg: convertMinutesToTextualHour(200) = 3 hour(s) and 20 minute(s)
   convertMinutesToTextualHour(minutes) {
-    return `${Math.floor(minutes / 60)} hour(s) and ${minutes % 60} minute(s)`;
+    return `${Math.floor(minutes / MINUTES_IN_ONE_HOUR)} hour(s) and ${
+      minutes % MINUTES_IN_ONE_HOUR
+    } minute(s)`;
   }
 
   // Returns the quarter (1 to 4) of the year for the given date, else use this.date
   getQuarter(newDate, date = this.date) {
     if (newDate) {
-      return Math.floor((new Date(newDate).getMonth() + 3) / 3);
+      return Math.floor(
+        (new Date(newDate).getMonth() + MONTHS_IN_QUARTER) / MONTHS_IN_QUARTER
+      );
     }
-    return Math.floor((date.getMonth() + 3) / 3);
+    return Math.floor(
+      (date.getMonth() + MONTHS_IN_QUARTER) / MONTHS_IN_QUARTER
+    );
   }
 
   // Returns current day (three letters, Mon through Sun) for the given date, else use this.date
   shortDay(newDate, date = this.date) {
     if (newDate) {
-      return WEEK_DAYS[new Date(newDate).getDay()].slice(0, 3);
+      return WEEK_DAYS[new Date(newDate).getDay()];
     }
-    return WEEK_DAYS[date.getDay()].slice(0, 3);
+    return WEEK_DAYS[date.getDay()];
   }
 }
-
-
-
